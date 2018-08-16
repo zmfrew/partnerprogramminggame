059af81fa6b738165d22556c4fb61308787a4299
zfrew <zmfrew@gmail.com>
2018-08-16 10:49:45 -0600

    Build out view controller functionality

---
+++ PartnerProgrammingGame/Helpers/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Contstants.Swift
+//  PartnerProgrammingGame
+//
+//  Created by Zachary Frew on 8/16/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+    
+    
+    static let pink = UIColor(red: 255/255, green: 126/255, blue: 121/255, alpha: 1)
+    static let blue = UIColor(red: 118/255, green: 214/255, blue: 255/255, alpha: 1)
+    static let green = UIColor(red: 0/255, green: 250/255, blue: 146/255, alpha: 1)
+    static let yellow = UIColor(red: 255/255, green: 252/255, blue: 121/255, alpha: 1)
+    
+    static let colors = [pink, blue, green, yellow]
+    
+    
+}
