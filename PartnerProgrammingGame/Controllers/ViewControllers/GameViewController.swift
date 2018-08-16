7dd61911f0b6496761432d1bc9d5de009c8af7eb
zfrew <zmfrew@gmail.com>
2018-08-16 09:56:31 -0600

    Build UI in storyboard and connect outlets

---
+++ PartnerProgrammingGame/Controllers/ViewControllers/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  PartnerProgrammingGame
+//
+//  Created by Zachary Frew on 8/16/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var pinkView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    // MARK: - Instance Properties
+    
+    
+    // MARK: - LifeCycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+
+}
