5213434e3daf82be2c3c2cfd77ec9442f7d08d23
zfrew <zmfrew@gmail.com>
2018-08-16 09:20:18 -0600

    Create User model

---
+++ PartnerProgrammingGame/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  PartnerProgrammingGame
+//
+//  Created by Zachary Frew on 8/16/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    
+    
+}
